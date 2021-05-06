48993c43327732777b647df6a3439c9530fb994c
Brandon-Magofna <magofna68@gmail.com>
2021-05-06 15:13:39 -0700

    finish up project
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

--- Find-And-Replace.Solution/Find-And-Replace/Models/Find-And-Replace.cs
+++ Find-And-Replace.Solution/Find-And-Replace/Models/Find-And-Replace.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+
 namespace FindAndReplaceSolution
 {
   public class FindAndReplace
   {
-
+    public static string FindAndReplaceAll(string sentence, string findWord, string replaceWord)
+    {
+      string modifiedSentence = "";
+      // look at each word in the sentence
+      foreach (string word in sentence.Split(' '))
+      {
+        // check if the current word in the sentence is the same as the findWord
+        if (findWord != word)
+        {
+          modifiedSentence += " " + word;
+        }
+        else if (findWord == word)
+        {
+          modifiedSentence += " " + replaceWord;
+        }
+      }
+      return modifiedSentence.Remove(0, 1);
+    }
   }
 }
---
+++ Find-And-Replace.Solution/Find-And-Replace/Program.cs
@@ -0,0 +1,29 @@
+using FindAndReplaceSolution;
+using System;
+using static System.Console;
+public class Program
+{
+  public static void Main()
+  {
+    BackgroundColor = ConsoleColor.Red;
+    Write("Welcome to ");
+    BackgroundColor = ConsoleColor.Yellow;
+    ForegroundColor = ConsoleColor.Blue;
+    Write("Thunderdome");
+    BackgroundColor = ConsoleColor.Red;
+    ForegroundColor = ConsoleColor.White;
+    Write("!\n");
+    BackgroundColor = ConsoleColor.Black;
+    WriteLine("We're gonna change some sentences!");
+    Write("What sentence shall we use? : ");
+    string sentence = ReadLine();
+    WriteLine(sentence);
+    Write("And what word should we replace? : ");
+    string findWord = ReadLine();
+    Write("And finally, what should we replace it with? : ");
+    string replaceWord = ReadLine();
+    string output = FindAndReplace.FindAndReplaceAll(sentence, findWord, replaceWord);
+    ForegroundColor = ConsoleColor.Blue;
+    Write(output);
+  }
+}
