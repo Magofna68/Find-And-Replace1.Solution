66d7e654993ecdb24ad34a4ee6e80e85b30abc3d
Brandon-Magofna <magofna68@gmail.com>
2021-05-06 14:20:34 -0700

    create first method and failing test
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>

--- Find-And-Replace.Solution/Find-And-Replace.Tests/ModelTests/Find-And-Replace.Tests.cs
+++ Find-And-Replace.Solution/Find-And-Replace.Tests/ModelTests/Find-And-Replace.Tests.cs
@@ -1,15 +1,28 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName;
+using FindAndReplaceSolution;
 
 namespace FindAndReplaceSolution.Tests //Change to match Title
 {
   [TestClass]
   public class FindAndReplaceTests
   {
-    // // [TestMethod]
+    // [TestMethod]
     // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
     // {
     //   // any necessary logic to prep for test; instantiating new classes, etc.
     //   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    [TestMethod]
+    public void FindAndReplaceAll_HelloWorld_Hello_World_WorldWorld()
+    {
+      // Arrange
+      string sentence = "Hello World";
+      string findWord = "Hello";
+      string replaceWord = "World";
+      string expectedOutputString = "World World";
+      // Act
+      string actualOutputString = FindAndReplace.FindAndReplaceAll(sentence, findWord, replaceWord);
+      // Assert
+      Assert.AreEqual(expectedOutputString, actualOutputString);
+    }
   }
 }
